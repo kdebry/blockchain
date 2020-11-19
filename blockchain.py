bb5e414517cb3070461e9637dcb71ebc6685999b
kdebry <debry.kevin@gmail.com>
2020-11-19 10:04:05 +0100

    mine function

--- blockchain.py
+++ blockchain.py
@@ -19,7 +19,7 @@ class Block:
     previous_hash : string
         Hash of the previous block in the chain which this block is part of
     """
-    def __init__(self, index, transactions, timestamp, previous_hash):
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
         """
         Parameters
         ----------
@@ -36,6 +36,7 @@ class Block:
         self.transactions = transactions
         self.timestamp = timestamp
         self.previous_hash = previous_hash
+        self.nonce = nonce
 
     def compute_hash(self):
         """
@@ -47,10 +48,15 @@ class Block:
 
 
 class Blockchain:
+    # difficulty of PoW algorithm
+    difficulty = 2
+
+
     def __init__(self):
         """
         Constructor for the Blockchain class
         """
+        self.unconfirmed_transactions = [] # data yet to get into blockchain
         self.chain = []
         self.create_genesis_block()
 
@@ -60,7 +66,8 @@ class Blockchain:
         the chain. The block has index 0, previous_hash as 0, and
         a valid hash.
         """
-        genesis_block = Block(0, [], time.time, 0)
+        genesis_block = Block(0, [], time.time, "0")
+        # there is no hash attribut.
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
     
@@ -72,3 +79,70 @@ class Blockchain:
         (i.e., genesis block)
         """
         return self.chain[-1]
+
+    @staticmethod
+    def proof_of_work(self, block):
+        """
+        Function that tries different values of the nonce to get a hash
+        that satisfies our difficulty criteria.
+        """
+        block.nonce = 0  # ? already set at 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification.
+        Verification includes:
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of 
+        a latest block in the chain match.
+        """
+        previous_hash = self.last_block.hash  # hash and func 
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        """
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out proof of work.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
